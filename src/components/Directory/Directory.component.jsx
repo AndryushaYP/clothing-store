f704e97408175ebbe553f1d5c662ddacad3562e9
Andrey Kloninger <veto.aion@gmail.com>
2021-08-16 14:22:29 +0500

    [add]: MenuItem withRouter

--- src/components/Directory/Directory.component.jsx
+++ src/components/Directory/Directory.component.jsx
@@ -39,8 +39,8 @@ const Directory = () => {
   ];
   return (
     <section className="directory-menu">
-      {sections.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      {sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
       ))}
     </section>
   );
