4435244a2626bd047a0f66e10b94bc7474578840
Andrey Kloninger <veto.aion@gmail.com>
2021-08-16 15:49:18 +0500

    [add]: Header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage/Homepage.component";
 import ShopPage from "./pages/ShopPage/ShopPage.component";
+import Header from "./components/Header/Header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.styles.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <nav className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/contact">
+          CONTACT
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
