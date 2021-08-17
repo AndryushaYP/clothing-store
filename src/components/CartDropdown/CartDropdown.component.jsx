7c4e1c2fd1f4ca0b4a4070815cad3d0df943861a
Andrey Kloninger <veto.aion@gmail.com>
2021-08-17 16:08:24 +0500

    [add]: CartItem

--- src/components/CartDropdown/CartDropdown.component.jsx
+++ src/components/CartDropdown/CartDropdown.component.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./CartDropdown.styles.scss";
 import CustomButton from "../CustomButton/CustomButton.component";
+import CartItem from "../CartItem/CartItem.component";
+import { useSelector } from "react-redux";
 
 const CartDropdown = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
   return (
     <section className="cart-dropdown">
-      <ul className="cart-items"></ul>
+      <ul className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </ul>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </section>
   );
--- src/components/CartIcon/CartIcon.component.jsx
+++ src/components/CartIcon/CartIcon.component.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./CartIcon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 const CartIcon = () => {
   const dispatch = useDispatch();
+  const count = useSelector((state) => state.cart.cartItems.length);
   return (
     <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{count}</span>
     </div>
   );
 };
